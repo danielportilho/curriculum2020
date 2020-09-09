ed5de63f14277e75027dcd049960e8424101b98d
Daniel de Souza Moreira Portilho <vital.dportilho@terceiros.mag.com.br>
2020-09-09 00:12:58 -0300

    pre-estrutura de html e css

--- curriculo2020/src/app/elements/elements.module.ts
+++ curriculo2020/src/app/elements/elements.module.ts
@@ -4,18 +4,21 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
+import { ScreenComponent } from './screen/screen.component';
 
 @NgModule({
   declarations: [
     NavbarComponent, 
     ContentComponent, 
-    FooterComponent
+    FooterComponent, 
+    ScreenComponent
   ],
 
   exports: [
     NavbarComponent, 
     ContentComponent, 
-    FooterComponent
+    FooterComponent,
+    ScreenComponent
   ],
 
   imports: [
